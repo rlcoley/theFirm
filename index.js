22a12a58b00e9afdf9d3712346de3f0c6c1c1dd6
rlcoley <rosscoley1002@gmail.com>
2018-10-22 16:42:31 -0400

    grabbing data from object

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var employeeName = document.querySelector('.firstlast');
 var employeeYears = document.querySelector('.yearsonjob');
 var employeeCity = document.querySelector('.cityofemployment');
+var budget = 810;
+var employees;
+var change;
 
 $.ajax({url:"https://bitbucket.org/OggiDanailov/firm/raw/2df585250847781831c5ab8ab4a7fdff8f5ef8fc/finances.json",
 // data: {t:searchTitle.value},
 success: function(response) {
   theFirmInfo = JSON.parse(response);
   console.log(theFirmInfo);
-  console.log(theFirmInfo.employees);
+  employees = theFirmInfo.employees;
+  change = theFirmInfo.change
+  console.log(change);
+  console.log(employees);
+  // console.log(Object.values(change))
 
 }
 })
