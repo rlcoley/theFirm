b7fa4558a3db475f45767f11d96173eaee7caa72
rlcoley <rosscoley1002@gmail.com>
2018-10-23 10:33:26 -0400

    object key 'promotion' added with appropriate text in keypair feilds

--- index.js
+++ index.js
@@ -12,16 +12,9 @@ function initialEmployeeArrayCreation(){
 		success: function(response) {
 			let theFirmInfo = JSON.parse(response);
 			console.log(theFirmInfo);
-
   			var theFirmInfoEmployees = objectSort(theFirmInfo.employees,"fname"); //the employees get sorted and isolated and put in a temporary variable
 	  		for(let i=0; i<theFirmInfoEmployees.length;i++){ //the employee info gets combined with their change and pushed to the global array for all of the employees.
-	  		let promotionHolder = ""; //will hold a string for whether or not they are up for a promotion before it is passed into the the array as part of the object
-			if((theFirmInfo["change"][theFirmInfoEmployees[i]["fname"]])/810>.3){
-				promotionHolder = "Up for a promotion";
-			} else {
-				promotionHolder = "Not up for a promotion";
-			}
-	  			fullEmployeeRoster.push({"fname" : theFirmInfoEmployees[i]["fname"], "lname" : theFirmInfoEmployees[i]["lname"],"city" : theFirmInfoEmployees[i]["city"],"experience":theFirmInfoEmployees[i]["experience"], "change": theFirmInfo["change"][theFirmInfoEmployees[i]["fname"]],"promotion" : promotionHolder});
+	  			fullEmployeeRoster.push({"fname" : theFirmInfoEmployees[i]["fname"], "lname" : theFirmInfoEmployees[i]["lname"],"city" : theFirmInfoEmployees[i]["city"],"experience":theFirmInfoEmployees[i]["experience"], "change": theFirmInfo["change"][theFirmInfoEmployees[i]["fname"]]});
 	  		}
 	  		console.log(fullEmployeeRoster);
 		}
@@ -47,4 +40,6 @@ function objectSort (toBeSorted, keyName) { //arg is an an array of objects to b
         }
     }
     return toBeSorted;
-}	
+
+    // console.log(fullEmployeeRoster[fname] + " " + fullEmployeeRoster[lname]);
+  }
