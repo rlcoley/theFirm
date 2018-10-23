8dce18db6dd518da36f327a74f0798271ece1efc
rlcoley <rosscoley1002@gmail.com>
2018-10-23 13:13:31 -0400

    made city array and arrays for each city

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ var employeeName = document.querySelector('.firstlast');
 var employeeYears = document.querySelector('.yearsonjob');
 var employeeCity = document.querySelector('.cityofemployment');
 var fullEmployeeRoster = [];
-
+var cities = []
+var fromPhilly = []
+var fromBarcelona = []
+var fromSofia = []
+var fromNYC = []
+var fromBerlin = []
+var fromWarsaw = []
 var employeeExperience = document.querySelector('.yearsofexperiences');
 
 
@@ -16,21 +22,56 @@ function initialEmployeeArrayCreation(){
 	  		for(let i=0; i<theFirmInfoEmployees.length;i++){ //the employee info gets combined with their change and pushed to the global array for all of the employees.
 	  			fullEmployeeRoster.push({"fname" : theFirmInfoEmployees[i]["fname"], "lname" : theFirmInfoEmployees[i]["lname"],"city" : theFirmInfoEmployees[i]["city"],"experience":theFirmInfoEmployees[i]["experience"], "change": theFirmInfo["change"][theFirmInfoEmployees[i]["fname"]]});
 	  		}
-	  		console.log(fullEmployeeRoster);
+	  		// console.log(fullEmployeeRoster);
+        getCities()
 		}
 	})
 }
 
+function getCities() {
+
+  for (var i = 0; i < fullEmployeeRoster.length; i++) {
+    cities.push({
+      city: fullEmployeeRoster[i].city
+    })
+    if (fullEmployeeRoster[i].city == "Philadelphia") {
+      fromPhilly.push(fullEmployeeRoster[i])
+    }if (fullEmployeeRoster[i].city == "Barcelona") {
+      fromBarcelona.push(fullEmployeeRoster[i])
+    }if (fullEmployeeRoster[i].city == "Sofia") {
+      fromSofia.push(fullEmployeeRoster[i])
+    }if (fullEmployeeRoster[i].city == "NYC") {
+      fromNYC.push(fullEmployeeRoster[i])
+    }if (fullEmployeeRoster[i].city == "Warsaw") {
+      fromWarsaw.push(fullEmployeeRoster[i])
+    }else if(fullEmployeeRoster[i].city == "Berlin") {
+      fromBerlin.push(fullEmployeeRoster[i])
+    }
+  }
+
+  // for (var i = 0; i < cities.length; i++) {
+  //   console.log(cities[i]);
+  // }
+  console.log(fromBarcelona);
+  console.log(fromBerlin);
+  console.log(fromNYC);
+  console.log(fromSofia);
+  console.log(fromPhilly);
+  console.log(fromWarsaw);
+
+  // console.log(fullEmployeeRoster);
+  // }
+}
 initialEmployeeArrayCreation();
 
 function objectSort (toBeSorted, keyName) { //arg is an an array of objects to be sorted and the key to sort by
-	console.log(toBeSorted[1][keyName]);
-	console.log(keyName);
+	// console.log(toBeSorted[1][keyName]);
+	// console.log(keyName);
     var holder1 = {}; //will hold the objects to be switched
     var holder2 = {}; //other holder
     for(var i=0;i<toBeSorted.length-1;i++){
         for(let j = 0; j<toBeSorted.length-(i+1);j++){
-        	console.log("toBeSorted "+toBeSorted[j][keyName]);
+        	// console.log("toBeSorted "+toBeSorted[j][keyName]);
             if(toBeSorted[j][keyName]>toBeSorted[j+1][keyName]){
                 holder1=toBeSorted[j];
                 holder2=toBeSorted[j+1]
@@ -39,7 +80,7 @@ function objectSort (toBeSorted, keyName) { //arg is an an array of objects to b
             }
         }
     }
+    // console.log(fullEmployeeRoster["fname"] + fullEmployeeRoster['lname']);
     return toBeSorted;
 
-    // console.log(fullEmployeeRoster[fname] + " " + fullEmployeeRoster[lname]);
   }
